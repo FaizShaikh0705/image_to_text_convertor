03496b34cff5a2fdbdbda9fd783bed56c496d352
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-01-28 19:52:22 +0530

    package update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
-import {FaHome} from 'react-icons/fa'
-import {BsCardHeading} from 'react-icons/bs'
-import {FcAbout} from 'react-icons/fc'
-import {BiConversation} from 'react-icons/bi'
-import {AiFillContacts} from 'react-icons/ai'
+// import {FaHome} from 'react-icons/fa'
+// import {BsCardHeading} from 'react-icons/bs'
+// import {FcAbout} from 'react-icons/fc'
+// import {BiConversation} from 'react-icons/bi'
+// import {AiFillContacts} from 'react-icons/ai'
+
+// const data [
+//   {
+//     id: 1,
+//     title: 'Home',
+//     icon: 'react-icons/fa'
+//     url: '#'
+//   },
+//   {
+//     id: 1,
+//     title: 'Home',
+//     icon: 'react-icons/fa'
+//     url: '#'
+//   },
+//   {
+//     id: 1,
+//     title: 'Home',
+//     icon: 'react-icons/fa'
+//     url: '#'
+//   },
+//   {
+//     id: 1,
+//     title: 'Home',
+//     icon: 'react-icons/fa'
+//     url: '#'
+//   },
+//   {
+//     id: 1,
+//     title: 'Home',
+//     icon: 'react-icons/fa'
+//     url: '#'
+//   }
+// ]
 
 const Nav = () => {
   return (
-    <nav>
-        <a href="#"><FaHome /></a>
-        <a href="#header"><BsCardHeading /></a>
-        <a href="#about"><FcAbout /></a>
-        <a href="#fa&q"><BiConversation /></a>
-        <a href="#contact"><AiFillContacts /></a>
+    <nav className='container container_navbar'>
+       <img src="" alt="" className="navbar_logo" />
     </nav>
   )
 }
